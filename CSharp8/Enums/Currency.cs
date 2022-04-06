fc8e98a27f8d409e7eaacfef68e6528d668f6287
Henk <henk.brink1985@gmail.com>
2022-04-06 17:41:29 +1200

    First commit

---
+++ CSharp8/Enums/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp8.Enums
+{
+    internal enum Currency
+    {
+        NZD = 0,
+        USD = 1,
+        AUD = 2,
+        ZAR = 3,
+        ARS = 4,
+        CAD = 5,
+        IDR = 6,
+        GBP = 7
+    }
+}
---
+++ CSharp8/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Top level statements
+//Global namespace
+//Global using directives
+//Implicit usings
+
+Console.WriteLine("Hello, World!");
+
